fcf9e4a253438ef9fc7b73cc24abb695a6b6c2e7
拨云见雾 <1353107913@qq.com>
2021-01-17 00:03:40 +0800

    轮播图

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,47 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template v-slot:center>蘑菇街</template>
+    </nav-bar>
+    <swiper>
+      <swiper-item v-for="(item, index) in banners.list" :key="index">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "home"
-    }
+  import { getMultiData } from '@/network/home'
+  import NavBar from 'components/common/navbar/Navbar'
+  import { Swiper, SwiperItem } from 'components/common/swiper'
+  export default {
+      name: "home",
+      components: {
+        NavBar,
+        Swiper,
+        SwiperItem
+      },
+      data () {
+        return {
+          banners: [],
+          recommends: []
+        }
+      },
+      created () {
+        getMultiData().then(res => {
+          this.banners = res.data.banner
+          this.recommends = res.data.recommend
+        })
+      }
+  }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
